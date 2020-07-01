1bd6088e3b73d13a0bc01a90608fb3877e5b904c
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-07-01 21:39:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+//Search by song or artist
+function searchSongs(term){
+    fetch(`${apiURL}/suggets/${term}`).then(res => res.json())
+    .then(data => console.log(data));
+}
+
+//Event listeners
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    if(!searchTerm){
+        alert('Please type in a search term');
+    } else {
+        searchSongs(searchTerm);
+    }
+});
